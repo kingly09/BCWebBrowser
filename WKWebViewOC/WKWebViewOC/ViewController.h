8cacf30096d8d4bbc6a3d0b7a2725958ecd7b68a
kingly <libintm@163.com>
2017-10-31 09:43:21 +0800

    初始化

---
+++ WKWebViewOC/WKWebViewOC/ViewController.h
@@ -0,0 +1,12 @@
+//
+//  ViewController.h
+//  WKWebViewOC
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WKWebViewOC/WKWebViewOC/ViewController.m
@@ -0,0 +1,65 @@
+//
+//  ViewController.m
+//  WKWebViewOC
+
+
+#import "ViewController.h"
+#import "WKWebViewController.h"
+
+@interface ViewController ()<UISearchBarDelegate>
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (assign,nonatomic) BOOL isPost;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)loadingAction:(UIButton *)sender {
+    
+    NSString *postData = @"{\"name\": \"libin\",\"password\": \"libin\"}";
+    WKWebViewController *web = [[WKWebViewController alloc] init];
+    [web POSTWebURLSring:_searchBar.text postData:postData];
+    [self.navigationController pushViewController:web animated:YES];
+}
+
+/**
+ 腾讯企业邮箱自动登录
+
+ @param sender UIButton
+ */
+- (IBAction)OnClickQiyeEmail:(id)sender {
+    
+    WKWebViewController *web = [[WKWebViewController alloc] init];
+    [web loadWebURLSring:@"https://m.exmail.qq.com/cgi-bin/loginpage"];
+    [self.navigationController pushViewController:web animated:YES];
+}
+
+
+#pragma mark 点击search跳到搜索结果页
+-(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    
+    WKWebViewController *web = [[WKWebViewController alloc] init];
+    [web loadWebURLSring:searchBar.text];
+    [self.navigationController pushViewController:web animated:YES];
+
+}
+
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    
+}
+
+
+
+
+@end
