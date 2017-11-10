f0d3de19d7ec7fd9cf330ccfc4bbe3faeee299f7
kingly <libintm@163.com>
2017-11-10 10:34:35 +0800

    初始化工程

---
+++ MX5BrowserOC/MX5BrowserOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MX5BrowserOC
+//
+//  Created by kingly on 2017/11/10.
+//  Copyright © 2017年 kingly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MX5BrowserOC/MX5BrowserOC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MX5BrowserOC
+//
+//  Created by kingly on 2017/11/10.
+//  Copyright © 2017年 kingly. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
